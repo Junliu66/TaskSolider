2a111ebe10393c841b5ac018097fd455ac4ff4a4
Junliu Zhang <junliuzhang@junlius-mbp.attlocal.net>
2020-05-18 00:58:53 -0700

    add authentication

--- task-app/src/components/task/TaskApp.jsx
+++ task-app/src/components/task/TaskApp.jsx
@@ -16,47 +16,77 @@ class LoginComponent extends Component {
     this.state = {
       username: "in28minutes",
       password: "",
+      hasLoginFailed: false,
+      showSuccessMessage: false,
     };
-    this.handleUserrnameChange = this.handleUsernameChange.bind(this);
-    this.handlePasswordChange = this.handlePasswordChange.bind(this);
-    
+    //this.handleUserrnameChange = this.handleUsernameChange.bind(this);
+    //this.handlePasswordChange = this.handlePasswordChange.bind(this);
+
+    this.handleChange = this.handleChange.bind(this);
+    this.loginClicked = this.loginClicked.bind(this);
   }
 
-  handleUsernameChange(event) {
-    console.log(event.target.value);
+  handleChange(event) {
+    //console.log(this.state);
     this.setState({
-      username: event.target.value,
+      [event.target.name]: event.target.value,
     });
   }
 
-  handlePasswordChange(event) {
-    console.log(event.target.value);
-    this.setState({
-      password: event.target.value,
-    });
+  loginClicked() {
+    if (
+      this.state.username === "in28minutes" &&
+      this.state.password === "dummy"
+    ) {
+      console.log("Successful");
+      this.setState({ showSuccessMessage: true });
+      this.setState({ hasLoginFailed: false });
+    } else {
+      this.setState({ showSuccessMessage: false });
+      this.setState({ hasLoginFailed: true });
+      console.log("Failed");
+    }
+
+    //console.log(this.state);
   }
 
   render() {
     return (
       <div>
+        <ShowInvalidCredentials hasLoginFailed={this.state.hasLoginFailed}/>
+        <ShowLoginSuccessMessage showSuccessMessage={this.state.showSuccessMessage}/>
         User Name:{" "}
         <input
           type="text"
           name="username"
           value={this.state.username}
-          onChange={this.handleUserrnameChange}
+          onChange={this.handleChange}
         />
         Password:{" "}
         <input
           type="password"
           name="password"
           value={this.state.password}
-          onChange={this.handlePasswordChange}
+          onChange={this.handleChange}
         />
-        <button>Login</button>
+        <button onClick={this.loginClicked}>Login</button>
       </div>
     );
   }
+}
+
+function ShowInvalidCredentials(props) {
+  if (props.hasLoginFailed) {
+    return <div>Invalid Credentials</div>;
+  }
+  return null
+}
+
+function ShowLoginSuccessMessage(props) {
+    if (props.showSuccessMessage) {
+        return <div>Login Successful</div>;
+    }
+    return null
 }
 
 export default TaskApp;

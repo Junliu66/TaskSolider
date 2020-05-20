05c6b22f27f994867c022a134b8b1268057ac639
Junliu Zhang <junliuzhang@junlius-mbp.attlocal.net>
2020-05-19 17:41:27 -0700

    add bootstrap

--- task-app/src/components/task/TaskApp.jsx
+++ task-app/src/components/task/TaskApp.jsx
@@ -28,9 +28,19 @@ class TaskApp extends Component {
 class HeaderComponent extends Component {
   render() {
     return (
-      <div>
-        Header <hr />
-      </div>
+      <header>
+        <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+          <div><a href="http://www.in28Minutes.com" className="navbar-brand">TaskSolider</a></div>
+          <ul className="navbar-nav">
+            <li><Link className="nav-link" to="/welcome/Junliu">Home</Link></li>
+            <li><Link className="nav-link" to="/tasks">Tasks</Link></li>  
+          </ul>
+          <ul className="navbar-nav navbar-collapse justify-content-end">
+            <li><Link className="nav-link" to="/login">Login</Link></li>
+            <li><Link className="nav-link" to="/logout">Logout</Link></li>  
+          </ul>      
+        </nav>  
+      </header>
     );
   }
 }
